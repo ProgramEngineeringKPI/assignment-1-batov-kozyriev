5b8451324979058cf645db1f7212443eb2791cfd
fluoxe7ine <locbkolocb@gmail.com>
2018-04-03 17:39:44 +0300

    added python implementation

---
+++ main.py
@@ -0,0 +1,40 @@
+import operator
+
+fin = open("eurovision.csv", "r") #input file
+fout = open("results.csv", "w") #output file
+
+count = fin.readline() #reading lines count
+countries = []
+matrix = []
+column = []
+
+def rate(col):
+    col.sort(reverse = True)
+    countries[col[0][1]][1] = countries[col[0][1]][1] + 12
+    countries[col[1][1]][1] = countries[col[1][1]][1] + 10
+    for i in range(2,10):
+        countries[col[i][1]][1] = countries[col[i][1]][1] + (10 - i)
+
+def output():
+    countries.sort(key=lambda x: x[1])
+    countries.reverse()
+    for i in range(int(count)):
+        fout.write(str(countries[i][0]) + ',' + str(countries[i][1]) + '\n')
+
+#creating arrays of countries and matrix
+for line in range(int(count)):
+    line = fin.readline()
+    buff = line.rstrip().split(',')
+    countries.append([buff[0], 0])
+    del(buff[0])
+    matrix.append(buff)
+
+for j in range(int(count)):
+    for i in range(int(count)):
+        buff = int(matrix[i][j])
+        column.append([buff, i])
+    rate(column)
+    column = []
+output()
+fin.close()
+fout.close()
